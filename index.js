77726fae7c4943d36c815862587cb9ede723f9b9
Abner Y. D. C. Canellas <abnercanellas@gmail.com>
2022-02-18 19:59:06 -0300

    firtSolution+CommentsFix

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-//  To respond to the exercice i imagineted a matrix with the dimensions of the land filled with 0s and the gold mines with 1s
-//and then i iterated the area from the top left corner. Each column iterating row by row to count the mines within the area
-//of the exploration, to, then, updating the best spot if the count is greater than the current best spot.
+//  *Author:Abner Canellas*
+//  *Date: 18/02/2022*
+//  *Version: 1.0*
+//  To respond to the exercice i imagineted a matrix with the dimensions of the land filled with 0s and the gold mine fields with 1s
+//Then i iterated the area from the top left corner. Each column iterate row by row to count the mines within the area
+//of the exploration, to, then, update the best spot if the count is greater than the current best spot.
 //  On a 6x4 area, a 2x2 exploration and coordinates {(1, 0), (5, 0), (2,2), (3, 3)}, as the exemple:
 //|0|1|0|0|0|1|
 //|0|0|0|0|0|0|
 //|0|0|1|0|0|0|
 //|0|0|0|1|0|0|
 // 
-//  To prevent the matrix out of bounds, i subtracted the exploration dimensions from the land dimensions.
+//  To prevent passing the matrix out of bounds, i subtracted the exploration dimensions from the land dimensions.
 //  To gain memory access time, i used an array insted of objects, since , at least in C, arrays are faster than objects 'cause
 // they are stored in the memory in a contiguous way.
 //
 //  i'm with a feeling that this algorithm is not the best way to solve this problem, so i'm will upload this version to mark the
 //time taken to solve the exercice with it.
-//  Because that, im thinking about a better way to solve this problem, probaly using a reduce function at a copy of a minor area, this way
+//  Beside that, im thinking about a better way to solve this problem, probaly using a reduce function at a copy of a minor area. This way
 //mey be faster. I'm not sure, but i believe that this can be done with the most part of this present algorithm and a most efficient 
 //way to crop/copy the explotable area.
 //  Other way that im thinking is catch the values of the top left and bottom right corners of the exploration and then sum the n mines within
